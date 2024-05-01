6c7e982967ddea200429af23d52a63649abe2c2f
m4rkbello <markbello10041999@gmail.com>
2024-05-01 22:17:29 +0800

    JS DOM: document.querySelector() and document.quirySelectorAll()

--- index.js
+++ index.js
@@ -13,8 +13,17 @@ let tagName = document.getElementsByTagName('h3');
 console.log(tagName);
 
 
-
 //getElementByClassName - kuhaon ang ClassName 
 let containerClassName = document.getElementById('containerClassName')
 let className = containerClassName.getElementsByClassName('className');
 console.log(className);
+
+
+//document.querySelector - kuhaon lang niya ang pinakauna na classname
+let queryselector = document.querySelector('.querySelector');
+console.log(queryselector);
+
+
+//document.querySelectorAll - kuhaon niya tanang id, classname, tags like div, h1, p ug daghan pa
+let querySelectorAll = document.querySelectorAll("#querySelectorAll, .querySelectorAllName, section  h3 h2");
+console.log(querySelectorAll);
