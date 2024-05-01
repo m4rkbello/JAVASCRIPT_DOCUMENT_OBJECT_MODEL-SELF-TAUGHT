eec8097722a71b2009bdb63ca800df3d9d5c0fbd
m4rkbello <markbello10041999@gmail.com>
2024-05-01 23:18:38 +0800

    JS DOM: selecting parent element, first child, first child elment, last child, last child element, childNodes

--- index.js
+++ index.js
@@ -27,3 +27,17 @@ console.log(queryselector);
 //document.querySelectorAll - kuhaon niya tanang id, classname, tags like div, h1, p ug daghan pa
 let querySelectorAll = document.querySelectorAll("#querySelectorAll, .querySelectorAllName, section  h3 h2");
 console.log(querySelectorAll);
+
+
+//SELECTING PARTENT ELEMENT AND CHILD ELEMENT USING PARENTNODE
+let selectingChildElement = document.querySelector('.childElement');
+console.log(selectingChildElement.parentNode);
+
+
+//SELECTING PARTENT ELEMENT AND FIRST CHILD ELEMENT - KUHAON ANG FIRST CHILD
+let selectedParentElement = document.querySelector('.parentElementTest');
+console.log(selectedParentElement.firstChild); //KUHAON ANG FIRST CHILD NA INLINE SA PARENT ELEMENT
+console.log(selectedParentElement.firstElementChild); //E  DISPLAY ANG FIRST ELEMENT CHILD SULOD SA PARENT ELEMENT
+console.log(selectedParentElement.lastChild); //KUHAON ANG LAST CHILD NA INLINE SA PARENT ELEMENT
+console.log(selectedParentElement.lastElementChild) //E DISPLAY ANG LAST ELEMENT CHILD SULOD SA PARENT ELEMENT
+console.log(selectedParentElement.childNodes); //KUHAON TANANG DATA SA ELEMENTS SULOD SA PARENT ELEMENT
