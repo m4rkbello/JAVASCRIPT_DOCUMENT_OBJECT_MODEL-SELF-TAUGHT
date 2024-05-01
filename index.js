08c30d71efaa394ae8bb68235ced6af87b599ec5
m4rkbello <markbello10041999@gmail.com>
2024-05-01 23:35:38 +0800

    JS DOM: selecting sibling elements using previousElementSibling and nextElementSibling

--- index.js
+++ index.js
@@ -41,3 +41,10 @@ console.log(selectedParentElement.firstElementChild); //E  DISPLAY ANG FIRST ELE
 console.log(selectedParentElement.lastChild); //KUHAON ANG LAST CHILD NA INLINE SA PARENT ELEMENT
 console.log(selectedParentElement.lastElementChild) //E DISPLAY ANG LAST ELEMENT CHILD SULOD SA PARENT ELEMENT
 console.log(selectedParentElement.childNodes); //KUHAON TANANG DATA SA ELEMENTS SULOD SA PARENT ELEMENT
+
+
+
+//SELECTING SIBLINGS ELEMENTS - PREVIOUSE-ELEMENT
+let selectingSiblings = document.querySelector('.secondSiblingElements');
+console.log(selectingSiblings.previousElementSibling); //kuhaon niyang unang sibling element which is ( <h4 >AYAW KOL BATA PAKO KOL</h4>)
+console.log(selectingSiblings.nextElementSibling); //kuhaon niyang sunod sa gigamit sa querySelector which is sibling element  ( <h4 >ANG NATULOG NA SI JUN2</h4>)
