ca66f3af9c35cc5daa1eeaf058b415172f69a530
m4rkbello <markbello10041999@gmail.com>
2024-05-01 21:20:24 +0800

    DOM - getElementById, getElementsByName, getElementsByTagName, getElementsByClassName

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ let id = document.getElementById('padayonLangId');
 console.log(id);
 
 
-//getElementByName
+//getElementByName - kuhaon ang list sa name
 let name = document.getElementsByName('padayonLangClassName');
 console.log(name);
 
+
+//getElementsByTagName - kuhaon ang tagName
+let tagName = document.getElementsByTagName('h3');
+console.log(tagName);
+
+
+
+//getElementByClassName - kuhaon ang ClassName 
+let containerClassName = document.getElementById('containerClassName')
+let className = containerClassName.getElementsByClassName('className');
+console.log(className);
